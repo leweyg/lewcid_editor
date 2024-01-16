6afaf8bc72ab331ce275d7ea79d912e7e72e649f
Lewey Geselowitz <LeweyGeselowitz@gmail.com>
2024-01-16 13:32:55 -0800

    Starting on XRHandScrollFactory

---
+++ examples/jsm/webxr/XRHandScrollFactory.js
@@ -0,0 +1,21 @@
+
+import { Object3D } from 'three';
+import { XRHandPrimitiveModel } from './XRHandPrimitiveModel.js';
+import { XRHandMeshModel } from './XRHandMeshModel.js';
+
+class XRHandScrollPair {
+
+};
+
+class XRHandScrollState {
+
+};
+
+class XRHandScrollFactory {
+
+	constructor( controller ) {
+	}
+};
+
+export { XRHandScrollFactory };
+
